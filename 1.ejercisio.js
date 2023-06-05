15883dc8a25974c951e742aad3d619c7563f1183
ManuelAlejandro93 <134321003+ManuelAlejandro93@users.noreply.github.com>
2023-06-05 15:12:05 -0500

    Update se añadió contenido random en GitHub. 4ta línea.

--- 1.ejercisio.js
+++ 1.ejercisio.js
@@ -1,3 +1,4 @@
 console.log("Soy una niña muy bella, añañaña");
 console.log("Además de sabrosita.");
 console.log("He añadido contenido en GitHub.");
+console.log("Se añadió contenido random en GitHub.");
