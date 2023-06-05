bbdf175f0a881e3bebc9586974aed85159ff8a2b
Manuel Alejandro <clasefisica2019@gmail.com>
2023-06-05 15:17:18 -0500

    He agregado una suma

--- 1.ejercisio.js
+++ 1.ejercisio.js
@@ -2,3 +2,8 @@ console.log("Soy una niña muy bella, añañaña");
 console.log("Además de sabrosita.");
 console.log("He añadido contenido en GitHub.");
 console.log("Se añadió contenido random en GitHub.");
+
+//Commit de agragar una suma
+
+let suma = 1+1;
+console.log(suma);
